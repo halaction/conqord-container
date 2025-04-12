8063026daa476da915ea71ce0e543be005504180
Hleb Stenin <hlebstenin@gmail.com>
2025-04-12 22:28:26 +0000

    fix: try larger params and inputs

--- src/conqord/step1_supervised_finetuning_LM/run_step1.sh
+++ src/conqord/step1_supervised_finetuning_LM/run_step1.sh
@@ -17,7 +17,7 @@ deepspeed --master_port 13001 main.py \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --data_output_path ../datasets/datatmp/ \
-   --max_seq_len 128 \
+   --max_seq_len 512 \
    --learning_rate 1e-4 \
    --weight_decay 0. \
    --num_train_epochs 1 \
@@ -29,7 +29,7 @@ deepspeed --master_port 13001 main.py \
    --zero_stage 3 \
    --deepspeed \
    --offload \
-   --lora_dim 64 \
+   --lora_dim 128 \
    --lora_module_name "layers." \
    --output_dir checkpoint/step1 \
    --print_loss \
