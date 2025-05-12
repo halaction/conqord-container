559852f39c0712775da4c95bff6740761a20f6bd
Hleb Stenin <hlebstenin@gmail.com>
2025-05-12 20:37:59 +0000

    fix: minor refactoring

--- scripts/run_steps.sh
+++ scripts/run_steps.sh
@@ -5,19 +5,21 @@ export HF_HUB_CACHE="$HOME/.cache/huggingface/hub"
 export BASE_DIR="$HOME/conqord-container"
 export SCRIPTS_DIR="$BASE_DIR/scripts"
 export CONQORD_DIR="$BASE_DIR/src/conqord"
-export MODEL_ID="${MODEL_ID:-google/gemma-2b-it}"
-
 export HF_HUB_ENABLE_HF_TRANSFER=1
 
-echo "Downloading $MODEL_ID..."
-huggingface-cli download $MODEL_ID
+MODEL="${MODEL:-google/gemma-2b-it}"
+ORGANIZATION_NAME=$(echo "$MODEL" | cut -d "/" -f 1)
+MODEL_NAME=$(echo "$MODEL" | cut -d "/" -f 2)
+
+echo "Downloading $MODEL..."
+MODEL_PATH=$(huggingface-cli download $MODEL)
 
 echo "Running step 1..."
 cd "$CONQORD_DIR/step1_supervised_finetuning_LM"
-sh run_step1.sh
+MODEL_PATH="$MODEL_PATH" sh run_step1.sh
 
 echo "Uploading step 1 checkpoint..."
-REPO_ID="halaction/conqord-checkpoint-step1"
+REPO_ID="halaction/$MODEL_NAME-conqord-step1"
 LOCAL_PATH="./checkpoint/step1"
 huggingface-cli upload $REPO_ID $LOCAL_PATH . --repo-type model
 
--- src/conqord/step1_supervised_finetuning_LM/run_step1.sh
+++ src/conqord/step1_supervised_finetuning_LM/run_step1.sh
@@ -2,9 +2,6 @@
 
 # Step 1: Supervised finetuning LLM to output confidence
 
-MODEL_DIR="models--google--gemma-2b-it"
-SNAPSHOT_HASH="96988410cbdaeb8d5093d1ebdc5a8fb563e02bad"
-
 mkdir -p log
 mkdir -p checkpoint
 mkdir -p tensorboard
@@ -13,7 +10,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2
 deepspeed --master_port 13001 main.py \
    --data_path openai/webgpt_comparisons \
    --data_split 10,0,0 \
-   --model_name_or_path "$HF_HUB_CACHE/$MODEL_DIR/snapshots/$SNAPSHOT_HASH/" \
+   --model_name_or_path "$MODEL_PATH/" \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --data_output_path ../datasets/datatmp/ \
